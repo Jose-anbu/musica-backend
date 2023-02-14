44f40188398716fae0065497580a5b893c34691f
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-14 18:21:30 -0300

    Se agregan metodos get, post, put y delete para canciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,6 +19,23 @@ router.get("/artistas", async (req, res) => {
   }
 });
 
+/// OBTENER CANCIONES
+router.get("/canciones", async (req, res) => {
+  try {
+    const canciones = await db.collection("canciones").get();
+    const datos = canciones.docs.map((cancion) => {
+      return {
+        id: cancion.id,
+        ...cancion.data(),
+      };
+    });
+    res.send(datos);
+  } catch (error) {
+    res.status(500);
+    res.send("Ocurrió un error inesperado");
+  }
+});
+
 /// CARGAR DATOS
 router.post("/artistas", async (req, res) => {
   const body = req.body;
@@ -33,6 +50,18 @@ router.post("/artistas", async (req, res) => {
   res.send("Cargado con exito");
 });
 
+/// CARGAR CANCIONES
+router.post("/canciones", async (req, res) => {
+  const body = req.body;
+  await db.collection("canciones").add({
+    nombre: body.nombre,
+    duracion: body.duracion,
+    nombreDisco: body.nombreDisco,
+    imagenDisco: body.imagenDisco
+  });
+  res.send("Cargado con exito");
+});
+
 /// OBTENER UN ARTISTA
 router.get("/artistas/:id", async (req, res) => {
   const id = req.params.id;
@@ -40,17 +69,38 @@ router.get("/artistas/:id", async (req, res) => {
   res.send(datos.data());
 });
 
-/// MODIFICAR
+/// OBTENER UNA CANCION
+router.get("/canciones/:id", async (req, res) => {
+  const id = req.params.id;
+  const datos = await db.collection("canciones").doc(id).get();
+  res.send(datos.data());
+});
+
+/// MODIFICAR ARTISTA
 router.put("/artistas/:id", async (req, res) => {
   const id = req.params.id;
   await db.collection("artistas").doc(id).update(req.body);
   res.send("Datos modificados");
 });
 
-/// ELIMINAR
+/// MODIFICAR CANCION
+router.put("/canciones/:id", async (req, res) => {
+  const id = req.params.id;
+  await db.collection("canciones").doc(id).update(req.body);
+  res.send("Datos modificados");
+});
+
+/// ELIMINAR ARTISTA
 router.delete("/artistas/:id", async (req, res) => {
   const id = req.params.id;
   await db.collection("artistas").doc(id).delete();
+  res.send("Eliminado");
+});
+
+/// ELIMINAR CANCION
+router.delete("/canciones/:id", async (req, res) => {
+  const id = req.params.id;
+  await db.collection("canciones").doc(id).delete();
   res.send("Eliminado");
 });
 
