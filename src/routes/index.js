1afe564a1de321bc7e31b274be4a36201da9f280
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-14 13:24:51 -0300

    Se agrega try catch y cambiamos forEach por map

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,19 @@ const router = Router();
 const { db } = require("../firebase");
 
 router.get("/artistas", async (req, res) => {
-  const artistas = await db.collection("artistas").get();
-
-  console.log("Artistas: ", artistas.docs);
-  artistas.docs.forEach((artista) => {
-    console.log(artista.data())
-  });
-
-  res.send("Hola");
+  try {
+    const artistas = await db.collection("artistas").get();
+    const datos = artistas.docs.map((artista) => {
+      return {
+        id: artista.id,
+        ...artista.data(),
+      };
+    });
+    res.send(datos);
+  } catch (error) {
+    res.status(500);
+    res.send("Ocurrió un error inesperado");
+  }
 });
 
 module.exports = router;
