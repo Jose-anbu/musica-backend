55680b052f7a38a0ed2bc6b7bf2d0944d830f9a5
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-14 12:54:36 -0300

    Se modifica package.json. Se modifica index.js para traer datos cargados en firebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
-const { Router } = require("express")
-const router = Router()
-const { db } = require('../firebase')
+const { Router } = require("express");
+const router = Router();
+const { db } = require("../firebase");
 
-router.get('/artistas', (req, res)=>{
-    res.send('Probando artistas')
-})
+router.get("/artistas", async (req, res) => {
+  const artistas = await db.collection("artistas").get();
 
-module.exports=router
+  console.log("Artistas: ", artistas.docs);
+  artistas.docs.forEach((artista) => {
+    console.log(artista.data())
+  });
+
+  res.send("Hola");
+});
+
+module.exports = router;
