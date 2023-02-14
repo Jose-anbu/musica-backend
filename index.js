80e1d0d3d33e4e7601c3a7190f02e5a38d30a2b2
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-14 12:37:43 -0300

    Creacion de metodo get en index.js. Se importa index.js de routes en index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const routes=require('./src/routes/index')
+
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Escuchando puerto: ${process.env.PORT}`)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,9 @@
 const { Router } = require("express")
 const router = Router()
 const { db } = require('../firebase')
+
+router.get('/artistas', (req, res)=>{
+    res.send('Probando artistas')
+})
+
+module.exports=router
