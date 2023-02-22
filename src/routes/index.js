60b55239dc47b0f08f9cc14a1fa4c3f6b467b4f4
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-22 02:29:35 -0300

    Se comenta codigo que no se utiliza en base a los requerimientos del proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,47 +20,47 @@ router.get("/artistas", async (req, res) => {
 });
 
 /// OBTENER CANCIONES
-router.get("/canciones", async (req, res) => {
-  try {
-    const canciones = await db.collection("canciones").get();
-    const datos = canciones.docs.map((cancion) => {
-      return {
-        id: cancion.id,
-        ...cancion.data(),
-      };
-    });
-    res.send(datos);
-  } catch (error) {
-    res.status(500);
-    res.send("Ocurrió un error inesperado");
-  }
-});
+// router.get("/canciones", async (req, res) => {
+//   try {
+//     const canciones = await db.collection("canciones").get();
+//     const datos = canciones.docs.map((cancion) => {
+//       return {
+//         id: cancion.id,
+//         ...cancion.data(),
+//       };
+//     });
+//     res.send(datos);
+//   } catch (error) {
+//     res.status(500);
+//     res.send("Ocurrió un error inesperado");
+//   }
+// });
 
 /// CARGAR DATOS
-router.post("/artistas", async (req, res) => {
-  const body = req.body;
-  await db.collection("artistas").add({
-    nombre: body.nombre,
-    genero: body.genero,
-    pais: body.pais,
-    biografia: body.biografia,
-    imagen: body.imagen,
-    canciones: body.canciones,
-  });
-  res.send("Cargado con exito");
-});
+// router.post("/artistas", async (req, res) => {
+//   const body = req.body;
+//   await db.collection("artistas").add({
+//     nombre: body.nombre,
+//     genero: body.genero,
+//     pais: body.pais,
+//     biografia: body.biografia,
+//     imagen: body.imagen,
+//     canciones: body.canciones,
+//   });
+//   res.send("Cargado con exito");
+// });
 
 /// CARGAR CANCIONES
-router.post("/canciones", async (req, res) => {
-  const body = req.body;
-  await db.collection("canciones").add({
-    nombre: body.nombre,
-    duracion: body.duracion,
-    nombreDisco: body.nombreDisco,
-    imagenDisco: body.imagenDisco
-  });
-  res.send("Cargado con exito");
-});
+// router.post("/canciones", async (req, res) => {
+//   const body = req.body;
+//   await db.collection("canciones").add({
+//     nombre: body.nombre,
+//     duracion: body.duracion,
+//     nombreDisco: body.nombreDisco,
+//     imagenDisco: body.imagenDisco
+//   });
+//   res.send("Cargado con exito");
+// });
 
 /// OBTENER UN ARTISTA
 router.get("/artistas/:id", async (req, res) => {
@@ -77,31 +77,31 @@ router.get("/canciones/:id", async (req, res) => {
 });
 
 /// MODIFICAR ARTISTA
-router.put("/artistas/:id", async (req, res) => {
-  const id = req.params.id;
-  await db.collection("artistas").doc(id).update(req.body);
-  res.send("Datos modificados");
-});
+// router.put("/artistas/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await db.collection("artistas").doc(id).update(req.body);
+//   res.send("Datos modificados");
+// });
 
 /// MODIFICAR CANCION
-router.put("/canciones/:id", async (req, res) => {
-  const id = req.params.id;
-  await db.collection("canciones").doc(id).update(req.body);
-  res.send("Datos modificados");
-});
+// router.put("/canciones/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await db.collection("canciones").doc(id).update(req.body);
+//   res.send("Datos modificados");
+// });
 
 /// ELIMINAR ARTISTA
-router.delete("/artistas/:id", async (req, res) => {
-  const id = req.params.id;
-  await db.collection("artistas").doc(id).delete();
-  res.send("Eliminado");
-});
+// router.delete("/artistas/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await db.collection("artistas").doc(id).delete();
+//   res.send("Eliminado");
+// });
 
 /// ELIMINAR CANCION
-router.delete("/canciones/:id", async (req, res) => {
-  const id = req.params.id;
-  await db.collection("canciones").doc(id).delete();
-  res.send("Eliminado");
-});
+// router.delete("/canciones/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await db.collection("canciones").doc(id).delete();
+//   res.send("Eliminado");
+// });
 
 module.exports = router;
